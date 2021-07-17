f63d4b6d7bc9ddcb1a4eeb42a9b7ba5d1682b98d
AdriBusse <adri.busse@gmail.com>
2021-07-17 16:28:29 +0200

    add slug to category for searching

--- api/category/models/category.js
+++ api/category/models/category.js
@@ -1,8 +1,18 @@
+const slugify = require('slugify');
+
 module.exports = {
     lifecycles: {
         beforeCreate: async (data) => {
             if (!data.describtion) {
                 data.describtion = data.title;
+            }
+            if (data.title) {
+                data.slug = slugify(data.title);
+            }
+        },
+        beforeUpdate: async (params, data) => {
+            if (data.title) {
+                data.slug = slugify(data.title);
             }
         },
 
